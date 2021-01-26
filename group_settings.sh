d89481308e0b273c405596d21dd8625746bc407d
student <student@SoC-courses.localdomain>
2021-01-26 11:38:24 +0200

    Modified the execution of script file

--- group_settings.sh
+++ group_settings.sh
@@ -1,10 +1,10 @@
 #insert file name of the sequence here
-SEQUENCE=videofile.yuv
+SEQUENCE=mBosphorus_1920x1080_120fps_420_8bit_YUV.yuv
 #insert QP value here
-QP=50
+QP=27
 #insert ID of your group here
-GID=99
+GID=05
 #The paths to the videos, both input and output
 #Fix paths if working remotely to /home/student/COMP-*
-INPUT_PATH=/mnt/hgfs/COMP-CE-400-test-sequences
-OUTPUT_PATH=/mnt/hgfs/P/COMP-CE-400-videos
+INPUT_PATH=/home/student/COMP-CE-400-test-sequences
+OUTPUT_PATH=/home/student/COMP-CE-400-videos
