db3cf2bfd346f35526564e2ff8d70cad1e81d598
Quang Phan <quang.phan@tuni.fi>
2021-02-10 21:13:12 +0200

    Declear process 3 and 4 in header file

--- TUNI.COMP.CE.400/ip.sw/CrypterSimApp/1.0_student/process.hh
+++ TUNI.COMP.CE.400/ip.sw/CrypterSimApp/1.0_student/process.hh
@@ -34,3 +34,37 @@ SC_MODULE (process2)
 		reset_signal_is(reset, false);
 	}
 };
+
+SC_MODULE (process3)
+{
+	sc_in_clk	 clock; //Clock input
+	sc_in<bool>   reset; //Reset, active high
+	sc_fifo_in<sc_uint<32> > fifo; //Fifo where from the values are fed.
+	value1* memory; //The shared memory
+
+	void read_value ();
+
+	SC_CTOR(process3)
+	{
+		//Initialize process as a thread, with reset active high
+		SC_CTHREAD(read_value, clock.pos());
+		reset_signal_is(reset, false);
+	}
+};
+
+SC_MODULE (process4)
+{
+	sc_in_clk	 clock; //Clock input
+	sc_in<bool>   reset; //Reset, active high
+	sc_fifo_out<sc_uint<32> > fifo; //Fifo where the values are obtained.
+	sc_in<sc_uint<32> > out_value; //Value that is decrypted and output of the system.
+	
+	void decrypt ();
+
+	SC_CTOR(process4)
+	{
+		//Initialize process as a thread, with reset active high
+		SC_CTHREAD(decrypt, clock.pos());
+		reset_signal_is(reset, false);
+	}
+};
