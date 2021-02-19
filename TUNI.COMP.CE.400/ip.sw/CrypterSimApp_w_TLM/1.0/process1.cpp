3c5ad4ece7a2f3714e9d9ed74f44d002058d597e
Quang Phan <quang.phan@tuni.fi>
2021-02-19 23:47:26 +0200

    TLM version of CrypterSimApp

---
+++ TUNI.COMP.CE.400/ip.sw/CrypterSimApp_w_TLM/1.0/process1.cpp
@@ -0,0 +1,52 @@
+#include "process.hh"
+
+void process1::encrypt ()
+{
+	sc_uint<32> encrypted_value; //The encrypted value, is to be fed to the socket.
+
+	tlm::tlm_generic_payload* trans = new tlm::tlm_generic_payload; // TLM payload
+	sc_time delay = sc_time( 10, SC_NS ); // TLM payload delay	
+	
+	while( true )
+	{
+		//Inform that we're ready for next input value
+		//WAIT IS MANDATORY. Otherwise the feeder of input cannot proceed.
+		input_ready.notify();
+		wait();
+		
+		//Read input value...
+		encrypted_value = in_value;
+
+		//Switch places between first and last 16 bits.
+		encrypted_value = ( (encrypted_value.range( 15, 0 ) << 16 ) +
+		encrypted_value.range( 31, 16 ) );
+		//Encrypt the value with the key.
+		encrypted_value = encrypted_value ^ KEY;
+
+		// Set the payload attributes
+		trans->set_command( tlm::TLM_WRITE_COMMAND );
+		trans->set_address( addr.to_uint() );
+		trans->set_data_ptr( reinterpret_cast<unsigned char*>(&encrypted_value) );
+		trans->set_data_length( 4 );
+		trans->set_streaming_width( 4 );
+		trans->set_byte_enable_ptr( 0 );
+		trans->set_dmi_allowed( false );
+		trans->set_response_status( tlm::TLM_INCOMPLETE_RESPONSE );
+		
+		// Call the b_transport
+		socket->b_transport( *trans, sc_time( SC_ZERO_TIME ) );
+
+		// Realize the delay
+		wait( delay );
+
+		// Check the response status
+		if (trans->is_response_error() )
+  			SC_REPORT_ERROR("TLM-2", "Response error from b_transport");
+
+		// Advance the address index
+		if ( addr < MAX_ADDR )
+			addr++;
+		else
+			addr = 0;
+	}
+}
---
+++ TUNI.COMP.CE.400/ip.sw/CrypterSimApp_w_TLM/1.0/process2.cpp
@@ -0,0 +1,65 @@
+#include "process.hh"
+
+void process2::b_transport( tlm::tlm_generic_payload& trans, sc_time& delay )
+{
+	// Extract the attributes from the payload
+	tlm::tlm_command cmd = trans.get_command();
+	sc_dt::uint64    adr = trans.get_address() / 4;
+	unsigned char*   ptr = trans.get_data_ptr();
+	unsigned int     len = trans.get_data_length();
+	unsigned char*   byt = trans.get_byte_enable_ptr();
+	unsigned int     wid = trans.get_streaming_width();
+	
+	// Check that the payload is compatible
+	if (adr >= sc_dt::uint64(SIZE) || byt != 0 || len > 4 || wid < len)
+    		SC_REPORT_ERROR("TLM-2", "Target does not support given generic payload transaction");
+
+	// Actual operation
+	if ( ( cmd == tlm::TLM_READ_COMMAND ) && ( values_available > 0 ) )
+	{
+		memcpy(ptr, &values[adr], len);
+		values_available--;
+	}
+	else if ( ( cmd == tlm::TLM_WRITE_COMMAND ) && ( values_available < MAX_ADDR ) )
+	{
+		memcpy(&values[adr], ptr, len);
+		values_available++;
+		index = adr;
+	}
+
+	// Set the response status to OK
+	trans.set_response_status( tlm::TLM_OK_RESPONSE );
+}
+
+
+void process2::write_value ()
+{
+	sc_uint<32> read_value; //Value from the local memory
+	sc_uint<32> write_enable; //Becomes zero, when we may write.
+	
+	while( true )
+	{
+		// Wait for data to be ready
+		while ( values_available == 0 )
+		{
+			wait();		
+		}
+		
+		// Read the transfered value
+		read_value = values[index];
+		values_available--;
+
+		//Wait until we get write enable
+		//NOTICE: accessing shared memory WILL take clock cycles!
+		do
+		{
+			wait();
+			write_enable = memory->read( ENABLE_INDEX );
+		}
+		while ( write_enable == 1 );
+		
+		//Write value, set read enable
+		memory->write( read_value, VALUE_INDEX );
+		memory->write( 1, ENABLE_INDEX );
+	}
+}
