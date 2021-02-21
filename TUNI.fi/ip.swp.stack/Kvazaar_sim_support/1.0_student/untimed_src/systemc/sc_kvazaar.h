e9f0d929afabbe7d37983b9ba11020c602f0d7a5
Quang Phan <quang.phan@tuni.fi>
2021-02-21 19:34:14 +0200

    Kvazaar master header

--- TUNI.fi/ip.swp.stack/Kvazaar_sim_support/1.0_student/untimed_src/systemc/sc_kvazaar.h
+++ TUNI.fi/ip.swp.stack/Kvazaar_sim_support/1.0_student/untimed_src/systemc/sc_kvazaar.h
@@ -3,6 +3,9 @@
 #include <systemc.h>
 
 // TODO TLM includes
+#include <tlm.h>
+#include <tlm_utils/simple_initiator_socket.h>
+#include <tlm_utils/simple_target_socket.h>
 
 #include "global_supplement.h"
 
@@ -16,6 +19,16 @@ SC_MODULE (kvazaar)
 
 	// TLM socket master
     // TODO
+    tlm_utils::simple_initiator_socket<kvazaar> socket;
+
+    // TLM payload
+    tlm::tlm_generic_payload* trans = new tlm::tlm_generic_payload;
+
+    // TLM transaction delay
+    sc_time delay;
+
+    // Address array
+    sc_dt::uint64 mem_addr[8] = {UNFILT1_BASE, UNFILT2_BASE, ORIG_BLOCK_BASE, CONFIG_BASE, LAMBDA_BASE};
 
     // Original 64x64 block (LCU)
     unsigned char orig_block[4096];
@@ -56,8 +69,11 @@ SC_MODULE (kvazaar)
 	int flags_c;
 	
 	// Constructor
-    SC_CTOR(kvazaar) // TODO
+    SC_CTOR(kvazaar) : socket("socket") // TODO
     {
+		// Payload initiation		
+		delay = sc_time( 10, SC_NS );
+
 		simulation_error = 0;
 		
 		name = (char*)"/dev/kvazaar_accelerator";
