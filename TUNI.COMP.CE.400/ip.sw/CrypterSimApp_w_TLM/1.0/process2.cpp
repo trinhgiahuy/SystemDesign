57d54aa7a3de8e4f60bc957c305760aae9774bc6
Quang Phan <quang.phan@tuni.fi>
2021-02-23 12:57:42 +0200

    Correct the address and clean up

--- TUNI.COMP.CE.400/ip.sw/CrypterSimApp_w_TLM/1.0/process2.cpp
+++ TUNI.COMP.CE.400/ip.sw/CrypterSimApp_w_TLM/1.0/process2.cpp
@@ -4,7 +4,7 @@ void process2::b_transport( tlm::tlm_generic_payload& trans, sc_time& delay )
 {
 	// Extract the attributes from the payload
 	tlm::tlm_command cmd = trans.get_command();
-	sc_dt::uint64    adr = trans.get_address() / 4;
+	sc_dt::uint64    adr = trans.get_address();
 	unsigned char*   ptr = trans.get_data_ptr();
 	unsigned int     len = trans.get_data_length();
 	unsigned char*   byt = trans.get_byte_enable_ptr();
@@ -15,20 +15,21 @@ void process2::b_transport( tlm::tlm_generic_payload& trans, sc_time& delay )
     		SC_REPORT_ERROR("TLM-2", "Target does not support given generic payload transaction");
 
 	// Actual operation
-	if ( ( cmd == tlm::TLM_READ_COMMAND ) && ( values_available > 0 ) )
+	if ( cmd == tlm::TLM_READ_COMMAND && values_available > 0 )
 	{
-		memcpy(ptr, &values[adr], len);
+		memcpy(ptr, (&values[adr]), len);
 		values_available--;
 	}
-	else if ( ( cmd == tlm::TLM_WRITE_COMMAND ) && ( values_available < MAX_ADDR ) )
+	else if ( cmd == tlm::TLM_WRITE_COMMAND )
 	{
-		memcpy(&values[adr], ptr, len);
+		memcpy((&values[adr]), ptr, len);
 		values_available++;
 		index = adr;
 	}
 
 	// Set the response status to OK
 	trans.set_response_status( tlm::TLM_OK_RESPONSE );
+
 }
 
 
@@ -45,6 +46,8 @@ void process2::write_value ()
 			wait();		
 		}
 		
+		// printf("Value available %d\n", values_available.to_int());
+
 		// Read the transfered value
 		read_value = values[index];
 		values_available--;
