2f0181d5508713a3b52e8ea182511e1b9b33e8c4
Quang Phan <quang.phan@tuni.fi>
2021-02-23 12:56:56 +0200

    Convert data to int

--- TUNI.COMP.CE.400/ip.sw/CrypterSimApp_w_TLM/1.0/process1.cpp
+++ TUNI.COMP.CE.400/ip.sw/CrypterSimApp_w_TLM/1.0/process1.cpp
@@ -23,10 +23,13 @@ void process1::encrypt ()
 		//Encrypt the value with the key.
 		encrypted_value = encrypted_value ^ KEY;
 
+		//Convert data to b_transport
+		int data = encrypted_value.to_int();
+
 		// Set the payload attributes
 		trans->set_command( tlm::TLM_WRITE_COMMAND );
 		trans->set_address( addr.to_uint() );
-		trans->set_data_ptr( reinterpret_cast<unsigned char*>(&encrypted_value) );
+		trans->set_data_ptr( (unsigned char*)(&data) );
 		trans->set_data_length( 4 );
 		trans->set_streaming_width( 4 );
 		trans->set_byte_enable_ptr( 0 );
@@ -40,12 +43,12 @@ void process1::encrypt ()
 		wait( delay );
 
 		// Check the response status
-		if (trans->is_response_error() )
+		if ( trans->is_response_error() )
   			SC_REPORT_ERROR("TLM-2", "Response error from b_transport");
 
 		// Advance the address index
 		if ( addr < MAX_ADDR )
-			addr++;
+			addr += 1;			
 		else
 			addr = 0;
 	}
