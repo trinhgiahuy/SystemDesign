ac81db9226842a9e92be46b52f24015f61219b69
Quang Phan <quang.phan@tuni.fi>
2021-03-11 20:01:25 +0200

    Implement blinking effect

--- TUNI.COMP.CE.400/ip.sw/BlinkerApp/1.0_student/BlinkerApp.c
+++ TUNI.COMP.CE.400/ip.sw/BlinkerApp/1.0_student/BlinkerApp.c
@@ -4,12 +4,14 @@
 int main(int argc, char *argv[])
 {
 	//TODO: Initialize HAL layer
+	init_HAL();
 
 	while ( 1 )
 	{
 		usleep(1000);
 		
 		//TODO: Control LEDs with buttons
+		set_led( read_btn() );
 	}
 
 	return EXIT_SUCCESS;
