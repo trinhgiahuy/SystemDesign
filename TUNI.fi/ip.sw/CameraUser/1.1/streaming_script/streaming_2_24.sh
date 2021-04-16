ce34b95104ec90d37c89d3a4198f8f636dbf4e2b
Huy Trinh <giahuy.trinh@tuni.fi>
2021-04-16 12:30:10 +0300

    Create all streaming scripting files

---
+++ TUNI.fi/ip.sw/CameraUser/1.1/streaming_script/streaming_2_24.sh
@@ -0,0 +1,13 @@
+QP=24
+THREADS=2
+CAMERA_MODE=camera_high
+RESOLUTION=800x480
+
+ENCODER="Kvazaar_0 -i $CAMERA_MODE -o - --input-res $RESOLUTION --no-rdoq --no-sao --no-deblock \
+-q $QP --rd 0 -p 1 --full-intra-search --wpp --owf 1 --threads $THREADS --acc $THREADS \
+--vps-period 1 --pu-depth-intra 2-2"
+
+STREAMER="ffmpeg -loglevel error -re -stats -vcodec hevc -probesize 32 -analyzeduration 0 -i - \
+-an -vcodec copy -f mpegts -fflags nobuffer udp://192.168.0.1:8557?pkt_size=1880"
+
+./$ENCODER | ./$STREAMER
